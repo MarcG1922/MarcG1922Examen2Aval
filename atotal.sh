0a051e00a47ebba0cba70993ac4cdfb35973056a
Marc Granero <magrba@floridauniversitaria.es>
2024-02-20 09:04:23 +0100

    añadiendo atotal.sh

---
+++ atotal.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+while true; do
+    read -p "De que ciudad te gustaria ver los consumos? : " ciudad
+    if grep -q "$ciudad" consumos.txt; then
+        break
+    else
+        echo "Ciudad $ciudad no encontrada, por favor introduce otra ciudad.."
+    fi
+done
+
+total=$(grep "$ciudad" consumos.txt | awk '{ sum += $2 } END { print sum }')
+
+echo "El consumo total de $ciudad es $total ."
